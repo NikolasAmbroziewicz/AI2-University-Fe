54837a116cf7dd7c6d1a414ce6a83da061071e14
Nikolas Ambroziewicz <ambroziewicznikolas@wp.pl>
2024-01-06 12:46:29 +0100

    Lab E v2

--- lab-e/src/app/tasks/tasks.component.ts
+++ lab-e/src/app/tasks/tasks.component.ts
@@ -67,4 +67,21 @@ export class TasksComponent {
       this.ngOnInit();
     });
   }
+
+  canArchiveCompleted() {
+    for (const task of this.tasks) {
+      if (task.completed) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  canAddTask() {
+    if (this.isProcessing) {
+      return false;
+    }
+
+    return !!this.newTask.title;
+  }
 }
